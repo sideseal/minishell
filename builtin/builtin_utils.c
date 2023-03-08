3e6baef549034c637cb29082ef65d8294e55b60f
ckgun@naver.com <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>
2023-03-08 19:46:22 +0900

    modify: change pwd and oldpwd when cd executed

--- builtin/builtin_utils.c
+++ builtin/builtin_utils.c
@@ -6,7 +6,7 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:28:31 by gychoi            #+#    #+#             */
-/*   Updated: 2023/02/24 20:46:17 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/03/08 19:34:49 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,12 @@ void	free_strings(char *s1, char *s2)
 {
 	free(s1);
 	free(s2);
+}
+
+int	xnor_by_status(int n1, int n2, int status)
+{
+	if (n1 == n2 && n2 == status)
+		return (status);
+	else
+		return (!status);
 }
